3ed2b49faf11a292eaa789ca4bbf9531348917ae
peerawoot <peerawoot1767@gmail.com>
2025-08-10 11:28:46 +0700

    jsx to html

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
 //is a component
 function App() {
-  return <h1 class="text-3xl">Peerawoot</h1>; 
-}
+  const name = "Peerawoot";
+  const addresss = "Chonburi";
+
+  return (
+    //ถ้ามี html มากว่า 1 คำสั่งต้องมี <> </> , return คือการส่ง html ไปทำงานที่ main.jsx
+    <>
+      <h1>สวัสดีครับผม :{name}</h1>
+      <p>ที่อยู่ :{addresss} </p>
+    </>
+  );
+} //คำสั่ง js จะเขียนอยู่ใน {}
 
 export default App;
