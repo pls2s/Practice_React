900187b4c12a235f4ad2c1ea2687ebf9e8312199
peerawoot <peerawoot1767@gmail.com>
2025-08-10 13:30:32 +0700

    used image public/assets

--- src/components/PersonList.jsx
+++ src/components/PersonList.jsx
@@ -1,22 +1,27 @@
 import { useState } from "react";
+import boy from "../assets/boy.svg";
+import girl from "../assets/girl.svg";
+
 function PersonList() {
 
     const [data, _setData] = useState([
     { id: 1, name: "Peerawoot1", gender: "male" },
     { id: 2, name: "Peerawoot2", gender: "female" },
     { id: 3, name: "Peerawoot3", gender: "male" },
-    { id: 4, name: "Peerawoot4", gender: "female" } ]);
-    
+    { id: 4, name: "Peerawoot4", gender: "female" },
+    { id: 5, name: "Peerawoot5", gender: "male" }]);
+
     const [show, setShow] = useState(true);
     return (
         <>
             <h1>จำนวนประชากร {data.length}</h1>
             <button onClick={() => setShow(!show)}>{show ? "hide" : "show"}</button> {/*สลับการแสดงข้อมูล รีเทินเป็น t/f  , ? คือ if , : คือ else*/}
             
-            <ul>
+            <ul>  
                 {show && data.map((obj) => ( //map คือการวนลูป ข้อมูลทุกตัวใน data useState ใช้ show เพื่อสลับการแสดงข้อมูล 
                 <li key={obj.id}> {/*key เป็น id ของข้อมูล จะต้องใช้อันที่ข้อมูลไม่ช้ำ */}
-                    ชื่อ : {obj.name}, เพศ : {obj.gender}
+                    <img src={obj.gender === "male" ? boy : girl} width={40} height={40}/> {/*ถ้าเป็นชายให้แสดง boy ถ้าเป็นหญิงให้แสดง girl*/}
+                    ID : {obj.id} , ชื่อ : {obj.name} , เพศ : {obj.gender} 
                 </li> ))}
             </ul>
         </>
